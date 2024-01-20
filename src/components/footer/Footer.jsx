7e347d3290b7ec97f1f80f08c003e6995c91febe
Abdul Rafay <rafayzia3690@gmail.com>
2024-01-21 00:50:05 +0500

    experience added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import {IoLogoTwitter} from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href='#' className='footer__logo'>Ahsan Yousaf</a>
+      <a href='#' className='footer__logo'>Abdul Rafay Zia</a>
 
       <ul className='permalinks'>
         <li><a href='#'>Home</a></li>
@@ -26,7 +26,7 @@ const Footer = () => {
       </div>
 
       <div className="footer__copyright">
-        <small>&copy; Ahsan Yousaf. All rights reserved.</small>
+        <small>&copy; Abdul Rafay Zia. All rights reserved.</small>
       </div>
     </footer>
   )
